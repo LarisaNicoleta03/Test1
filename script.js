7c409dd4a6db774069c5ef897697e4bd4dbec633
LarisaNicoleta03 <160154527+LarisaNicoleta03@users.noreply.github.com>
2024-05-12 12:44:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openArticles() {
+    window.location.href = "index.html";
+}
+
+function openPlanete() {
+    window.location.href = "planete.html";
+}
+
+function openReviews() {
+    window.location.href = "reviews.html";
+}
+function openAnimation(){
+    window.location.href = "exemplu.html";
+}
+function openContact() {
+    window.location.href = "contact.html";
+}
